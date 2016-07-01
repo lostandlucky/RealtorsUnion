fc97e530c0567445152843f8375326977f25a71f
LostandLucky <stevenrday@gmail.com>
2016-07-01 07:24:18 -0400

    Added all attributes to Brokerage Table Model/Class

--- BrokerageInfo/models.py
+++ BrokerageInfo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Brokerage(models.Model)
+	BrokerageName = models.CharField(max_length=500)
+	#To-Do Fix additional settings for ImagesFields/FileFields
+	BrokerageLogo = ImageField
+	ReviewLink
+	ContactLink
+	TotalAgents = models.IntegerField
+	Location = models.CharField(max_length=500)
+	Desks = models.IntegerField
+	YearlyCosts = models.DecimalField(max_digits=12, decimal_places=2)
+	CommisionSplit = modles.CharField (max_length=8)
+	#To-Do set a getter for Cap that returns none
+	Cap =  models.DecimalField(max_digits=12, decimal_places=2)
+	TrainingPerWeek = models.IntegerField
+	Onboarding = models.BooleanField
+	Mentorship = models.BooleanField
+	Teams_Hiring = models.BooleanField
+	Marketing = models.CharField(max_length=500)
+	TotalListings = models.IntegerField
+	ConferenceRooms = models.BooleanField
+	OfficeLeaders = models.CharField (max_length 500)
+	OfficeLeaderPhoto = models.ImageField
+
