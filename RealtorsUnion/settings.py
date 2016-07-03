3d374df523b9e4d6236906cd79262334a4fd26f5
LostandLucky <stevenrday@gmail.com>
2016-07-02 20:25:48 -0400

    added reference to BrokerageInfo to manage.py file

--- RealtorsUnion/settings.py
+++ RealtorsUnion/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'Landing.apps.LandingConfig',
+    'BrokerageInfo.apps.BrokerageInfoConfig',
+    'Landing.apps.LandingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
